cbee17818b7c8f98ec96bd7d3671cd405d0aa798
vbaroboticesdrw <46609690+vbaroboticesdrw@users.noreply.github.com>
2019-03-01 15:10:38 -0600

    Update led.py

--- led.py
+++ led.py
@@ -1,10 +1,7 @@
-from gpiozero import LED
-from time import sleep
-
 led = LED(22)
+button = Button(4)
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+button.when_pressed = led.on
+button.when_released = led.off
+
+pause()
